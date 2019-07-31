aaf5108cf6c6039beeca19a9d161472250810c8e
Larrax <larraxik@gmail.com>
2019-07-31 08:59:37 +0200

    Updated simple resource monitor script

--- server/server_bin/measureUsage.py
+++ server/server_bin/measureUsage.py
@@ -5,22 +5,28 @@ import time
 import urllib3.request
 
 server = "http://localhost:5000"
+http = urllib3.PoolManager()
 
 while True:
-	cpu = psutil.cpu_percent()
-	mem = psutil.virtual_memory().percent
+	cpu = 0
+	mem = 0
 
-	hdd_read = psutil.disk_io_counters().read_bytes
-	hdd_write = psutil.disk_io_counters().write_bytes
-	recv = psutil.net_io_counters().bytes_recv
-	sent = psutil.net_io_counters().bytes_sent
+	hdd_read = psutil.disk_io_counters(nowrap=True).read_bytes
+	hdd_write = psutil.disk_io_counters(nowrap=True).write_bytes
+	recv = psutil.net_io_counters(nowrap=True).bytes_recv
+	sent = psutil.net_io_counters(nowrap=True).bytes_sent
 
-	time.sleep(60)
+	for i in range(60):
+		cpu += psutil.cpu_percent()
+		mem += psutil.virtual_memory().percent
+		time.sleep(1)
 
-	hdd_read = (psutil.disk_io_counters().read_bytes - hdd_read) / 60
-	hdd_write = (psutil.disk_io_counters().write_bytes - hdd_write) / 60
-	recv = (psutil.net_io_counters().bytes_recv - recv) / 60
-	sent = (psutil.net_io_counters().bytes_sent - sent) / 60
+	cpu /= 60
+	mem /= 60
+	hdd_read = (psutil.disk_io_counters(nowrap=True).read_bytes - hdd_read) / 60
+	hdd_write = (psutil.disk_io_counters(nowrap=True).write_bytes - hdd_write) / 60
+	recv = (psutil.net_io_counters(nowrap=True).bytes_recv - recv) / 60
+	sent = (psutil.net_io_counters(nowrap=True).bytes_sent - sent) / 60
 
 	hdd_read *= 8
 	hdd_write *= 8
@@ -41,9 +47,5 @@ while True:
 	print("HDD_R: " + str(hdd_read))
 	print("HDD_W: " + str(hdd_write))
 
-	http = urllib3.PoolManager()
-
 	url = server + "/serverInfo/saveData" + "?cpu=" + str(cpu) + "&ram=" + str(mem) + "&net_recv=" + str(recv) + "&net_sent=" + str(sent) + "&hdd_read=" + str(hdd_read) + "&hdd_write=" + str(hdd_write)
-
 	response = http.request('GET', url)
-
