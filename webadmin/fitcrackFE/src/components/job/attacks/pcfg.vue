37ef565e6eab88f30251e3c54b740dc84c5917fb
Tuxie1337 <franta01011000@gmail.com>
2019-08-28 19:35:34 +0200

    Make PCFG grammar from dictionary FE added.

--- webadmin/fitcrackFE/src/components/job/attacks/pcfg.vue
+++ webadmin/fitcrackFE/src/components/job/attacks/pcfg.vue
@@ -4,19 +4,35 @@
 -->
 
 <template>
+
   <div>
     <v-card-title class="pb-0"><h2>Select PCFG grammar<span class="required primary--text"> *</span></h2></v-card-title>
     <pcfg-selector v-model="pcfg" @input="checkValid"></pcfg-selector>
 
     <v-divider></v-divider>
-    <v-card-title class="pb-0"><h2>Select rule file</h2></v-card-title>
-    <rules-selector v-model="rules" @input="checkValid"></rules-selector>
+  <!--  <v-card-title class="pb-0"><h2>Select rule file</h2></v-card-title> -->
+  <!--  <rules-selector v-model="rules" @input="checkValid"></rules-selector> -->
+   <v-card-title class="pb-0"><h2>Edit keyspace limit</h2></v-card-title>
+    <v-card-text>
+      <v-text-field
+        flat
+        single-line
+        v-model="keyspaceLimit"
+        :value="this.keyspaceLimit"
+        required
+        type="number"
+        suffix="passwords"
+        :max="this.pcfg.keyspace"
+        @input="checkValid"
+      ></v-text-field>
+
+    </v-card-text>
   </div>
 </template>
 
 <script>
   import pcfgSelector from '@/components/selector/pcfgSelector'
-  import ruleSelector from '@/components/selector/rulesSelector'
+//  import ruleSelector from '@/components/selector/rulesSelector'
   export default {
     name: "pcfg",
     props: {
@@ -29,13 +45,15 @@
       value: function(){
         if (this.value.pcfg_grammar)
           this.pcfg = this.value.pcfg_grammar
-        if (this.value.rules)
-          this.rules = this.value.rules
+    /*    if (this.value.rules)
+          this.rules = this.value.rules */
+        if (this.pcfg)
+          this.keyspaceLimit = this.pcfg.keyspace
       }
     },
     components: {
       'pcfg-selector': pcfgSelector,
-      'rules-selector': ruleSelector
+  //    'rules-selector': ruleSelector
     },
     methods: {
       checkValid: function () {
@@ -45,13 +63,15 @@
           this.$emit('input', {
             'attack_mode': this.attackId,
             'attack_name': this.attackName,
-            'rules': this.rules,
-            'pcfg_grammar': this.pcfg
+      //      'rules': this.rules,
+            'pcfg_grammar': this.pcfg,
+            'keyspace_limit': this.keyspaceLimit
           })
           return true
       //  }
       //  return false
       }
+
     },
     data: function () {
       return {
@@ -59,7 +79,10 @@
         attackName: 'pcfg',
         valid: false,
         pcfg: [],
-        rules: null,
+    //    rules: null,
+        keyspaceLimit: 0,
+
+
       }
     }
   }
