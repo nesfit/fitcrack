4aa38bbacdf12c00e9c32c1eb59434763c05de6b
Lukáš Zobal <larraxik@gmail.com>
2019-11-12 09:34:00 +0100

    Added missing grpc headers

--- installer/build_server.sh
+++ installer/build_server.sh
@@ -41,6 +41,7 @@ cp -f server/src/source/Generators/*.cpp  boinc/sched/
 cp -f server/src/source/Database/*.cpp    boinc/sched/
 cp -f server/src/headers/protocol/*.cc    boinc/sched/
 cp -f server/src/headers/protocol/*.h     boinc/sched/
+cp -rf server/src/include/*               boinc/sched/
 
 rm -f tmp/built 2>/dev/null
 
