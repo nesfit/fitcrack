10e2232ad55ad5d89c39a4857bc6407bc5382dfc
OviOvocny <werint.pp@gmail.com>
2020-02-11 22:43:26 +0100

    add template table to schema

--- server/sql/10_create_tables.sql
+++ server/sql/10_create_tables.sql
@@ -317,6 +317,20 @@ CREATE TABLE IF NOT EXISTS `fc_job_graph` (
 -- --------------------------------------------------------
 
 --
+-- Štruktúra tabuľky pre tabuľku `fc_template`
+--
+
+CREATE TABLE `fc_template` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `template` json NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
 -- Štruktúra tabuľky pre tabuľku `fc_role`
 --
 
