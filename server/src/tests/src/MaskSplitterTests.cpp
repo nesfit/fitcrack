e432ddcf247ac4fb08bedcdd687baa79412537ab
Tomáš Ženčák <xzenca00@stud.fit.vutbr.cz>
2020-05-04 19:31:34 +0200

    Verify the test results using hashcat

--- server/src/tests/src/MaskSplitterTests.cpp
+++ server/src/tests/src/MaskSplitterTests.cpp
@@ -2,137 +2,214 @@
 
 #include <gtest/gtest.h>
 
+#include <fstream>
+
 static const std::string g_testMaskBasic = "a??b?dc?hd?He??f";
-static const std::string g_basicMaskCharsets[] = {
-	"0123456789",
-	"0123456789abcdef",
-	"0123456789ABCDEF"
-};
 using CharsetsType = std::vector<std::string>;
+
+std::vector<std::string> LoadMaskSuccession()
+{
+	std::ifstream input(TESTDATA_DIR "/generatedPasswords.txt");
+	std::string line;
+	std::vector<std::string> result;
+	while(std::getline(input, line))
+	{
+		result.push_back(std::move(line));
+	}
+	return result;
+}
+
+static std::vector<std::string> g_correctMasks = LoadMaskSuccession();
 
 TEST(Basic, ZeroOffsetAndKeyspace)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 0, 0);
+	size_t startingIndex = 0;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 0);
 	ASSERT_EQ(1, result.keyspace);
-	ASSERT_EQ("a??b0c0d0e??f", result.mask);
-	ASSERT_TRUE(result.customCharsets.empty());
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Basic, Offset1)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 1, 0);
+	size_t startingIndex = 1;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 0);
 	ASSERT_EQ(1, result.keyspace);
-	ASSERT_EQ("a??b1c0d0e??f", result.mask);
-	ASSERT_TRUE(result.customCharsets.empty());
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Basic, Keyspace2)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 0, 2);
+	size_t startingIndex = 0;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 2);
 	ASSERT_EQ(2, result.keyspace);
-	ASSERT_EQ("a??b?1c0d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[0].substr(0, 2)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Basic, FirstCharsetTwice)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 0, 20);
+	size_t startingIndex = 0;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 20);
 	ASSERT_EQ(20, result.keyspace);
-	ASSERT_EQ("a??b?dc?1d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[1].substr(0, 2)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Basic, FirstTwoCharsets)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 0, 160);
+	size_t startingIndex = 0;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 160);
 	ASSERT_EQ(160, result.keyspace);
-	ASSERT_EQ("a??b?dc?hd0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, BreakOnFirstCharset)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 4, 160);
+	size_t startingIndex = 4;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 160);
 	ASSERT_EQ(6, result.keyspace);
-	ASSERT_EQ("a??b?1c0d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[0].substr(4)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, BreakOnSecondCharset)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 20, 160);
+	size_t startingIndex = 20;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 160);
 	ASSERT_EQ(140, result.keyspace);
-	ASSERT_EQ("a??b?dc?1d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[1].substr(2)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, BreakOnFirstCharsetHugeKeyspace)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 4, 1600000);
+	size_t startingIndex = 4;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 1600000);
 	ASSERT_EQ(6, result.keyspace);
-	ASSERT_EQ("a??b?1c0d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[0].substr(4)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, BreakOnSecondCharsetHugeKeyspace)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 20, 1600000);
+	size_t startingIndex = 20;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 1600000);
 	ASSERT_EQ(140, result.keyspace);
-	ASSERT_EQ("a??b?dc?1d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[1].substr(2)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, BreakOnFirstCharsetSecondPartlyDone)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 24, 1600000);
+	size_t startingIndex = 24;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 1600000);
 	ASSERT_EQ(6, result.keyspace);
-	ASSERT_EQ("a??b?1c2d0e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[0].substr(4)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, BreakOnThirdCharset)
 {
 	MaskSplitter splitter;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 160*3, 9999999);
-	ASSERT_EQ(160*(g_basicMaskCharsets[2].size()-3), result.keyspace);
-	ASSERT_EQ("a??b?dc?hd?1e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[2].substr(3)}, result.customCharsets);
+	size_t startingIndex = 160*3;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 9999999);
+	ASSERT_EQ(160*(16-3), result.keyspace);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, NoSmallLeftovers1)
 {
 	MaskSplitter splitter;
-	size_t thirdCharsetLeftoverKeyspace = g_basicMaskCharsets[2].size()-3;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 160*3, 160*thirdCharsetLeftoverKeyspace-10);
+	size_t startingIndex = 160*3;
+	size_t thirdCharsetLeftoverKeyspace = 16-3;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 160*thirdCharsetLeftoverKeyspace-10);
 	ASSERT_EQ(160*thirdCharsetLeftoverKeyspace, result.keyspace);
-	ASSERT_EQ("a??b?dc?hd?1e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[2].substr(3)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(Advanced, NoSmallLeftovers2)
 {
 	MaskSplitter splitter;
-	size_t thirdCharsetLeftoverKeyspace = g_basicMaskCharsets[2].size()-3;
-	auto result = splitter.GetMaskSlice(g_testMaskBasic, 160*3, 160*thirdCharsetLeftoverKeyspace*0.6);
+	size_t startingIndex = 160*3;
+	size_t thirdCharsetLeftoverKeyspace = 16-3;
+	auto result = splitter.GetMaskSlice(g_testMaskBasic, startingIndex, 160*thirdCharsetLeftoverKeyspace*0.6);
 	ASSERT_EQ(160*(thirdCharsetLeftoverKeyspace/2), result.keyspace);
-	ASSERT_EQ("a??b?dc?hd?1e??f", result.mask);
-	ASSERT_EQ(CharsetsType{g_basicMaskCharsets[2].substr(3, thirdCharsetLeftoverKeyspace/2)}, result.customCharsets);
+	MaskSplitter testSplitter(result.customCharsets);
+	for(size_t i = 0; i < result.keyspace; ++i)
+	{
+		auto testRes = testSplitter.GetMaskSlice(g_testMaskBasic, i+startingIndex, 1);
+		ASSERT_EQ(g_correctMasks[i+startingIndex], testRes.mask);
+	}
 }
 
 TEST(CustomCharsets, Inclusion)
 {
 	MaskSplitter splitter({"123", "?1345"});
-	auto result = splitter.GetMaskSlice("?2as??d?2", 15, 9999999);
+	size_t startingIndex = 15;
+	auto result = splitter.GetMaskSlice("?2as??d?2", startingIndex, 9999999);
 	ASSERT_EQ(10, result.keyspace);
 	ASSERT_EQ("?2as??d?3", result.mask);
 	CharsetsType expectedCharsets{"123", "12345", "45"};
