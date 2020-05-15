33dcd45ed7588f6ebeef60017a6e72339d07b718
Dávid Bolvanský <david.bolvansky@gmail.com>
2020-05-15 09:28:29 +0200

    Fixed >100 progress for weird pcfg jobs with restarted pcfg manager

--- webadmin/fitcrackAPI/src/src/database/models.py
+++ webadmin/fitcrackAPI/src/src/database/models.py
@@ -382,7 +382,7 @@ class FcJobGraph(Base):
     def as_graph(self):
         return {
             'time': str(getattr(self, 'time')),
-            getattr(self.job, 'id'): round(getattr(self, 'progress'))
+            getattr(self.job, 'id'): min(round(getattr(self, 'progress')), 100)
         }
 
 
