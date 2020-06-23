1061c00a3c060dcabef2d46aa64bba0b43094f8f
Dávid Bolvanský <david.bolvansky@gmail.com>
2020-06-23 11:54:32 +0200

    Fixed boinc_connect for Debian

--- installer/boinc_connect.py
+++ installer/boinc_connect.py
@@ -9,11 +9,23 @@ import time
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--url", type=str, default="http://127.0.0.1/fitcrack")
-    args = parser.parse_args()    
-    subprocess.call(['boinccmd', '--project', args.url, 'detach'], cwd='/var/lib/boinc', stderr=subprocess.DEVNULL)
-    output = subprocess.check_output(['boinccmd', '--create_account', args.url, 'fitcrack@test.cz', 'fitcrack@test.cz', 'fitcrack@test.cz'], cwd='/var/lib/boinc')
+    args = parser.parse_args()
+
+    boinc_dir = None
+    if os.path.exists('/var/lib/boinc'):
+        # Almost all distros..
+        boinc_dir = '/var/lib/boinc'
+    elif os.path.exists('/var/lib/boinc-client'):
+        # Debian
+        boinc_dir = '/var/lib/boinc-client'
+    else:
+        print("Unable to find boinc folder", file=sys.stderr)
+        sys.exit(1)
+
+    subprocess.call(['boinccmd', '--project', args.url, 'detach'], cwd=boinc_dir, stderr=subprocess.DEVNULL)
+    output = subprocess.check_output(['boinccmd', '--create_account', args.url, 'fitcrack@test.cz', 'fitcrack@test.cz', 'fitcrack@test.cz'], cwd=boinc_dir)
     key = output.split()[-1]
-    ret = subprocess.call(['boinccmd', '--project_attach', args.url, key], cwd='/var/lib/boinc')
-    subprocess.call(['boinccmd', '--project', args.url, 'update'], cwd='/var/lib/boinc')
+    ret = subprocess.call(['boinccmd', '--project_attach', args.url, key], cwd=boinc_dir)
+    subprocess.call(['boinccmd', '--project', args.url, 'update'], cwd=boinc_dir)
     sys.exit(ret)
     
