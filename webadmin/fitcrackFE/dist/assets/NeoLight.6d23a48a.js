86fcfb0e6e82dd3ca531c326d6f438bfa8b2bbaf
Webpacker <me@ovi.moe>
2022-03-17 22:24:09 +0000

    Build webadmin frontend and Runner 🚀

--- webadmin/fitcrackFE/dist/assets/NeoLight.6d23a48a.js
+++
@@ -1,1 +0,0 @@
-var _="/assets/NeoDark.f777b749.svg",e="/assets/NeoLight.639e3782.svg";export{_,e as a};
---
+++ webadmin/fitcrackFE/dist/assets/logo-light.4fa25edf.js
@@ -0,0 +1,1 @@
+var _="/assets/logo-dark.f777b749.svg",e="/assets/logo-light.639e3782.svg";export{_,e as a};
