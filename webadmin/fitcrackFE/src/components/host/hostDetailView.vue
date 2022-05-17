5bad9d482c5fba01de2b0b6cf0323d90e75c4bb7
OviOvocny <werint.pp@gmail.com>
2022-05-17 10:33:29 +0200

    FE: add device info to host detail

--- webadmin/fitcrackFE/src/components/host/hostDetailView.vue
+++ webadmin/fitcrackFE/src/components/host/hostDetailView.vue
@@ -19,7 +19,7 @@
       <fc-tile
         title="Host info"
         :loading="data==null"
-        class=" max800 mb-5"
+        class="mb-5"
       >
         <v-list v-if="data != null">
           <v-list-item>
@@ -53,6 +53,50 @@
               <v-list-item-title>
                 {{ data.p_model }}
               </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </fc-tile>
+
+      
+      <fc-tile
+        title="Devices"
+        :loading="data === null"
+        class="mb-5"
+      >
+        <v-list v-if="data != null">
+          <v-list-item v-for="device in data.devices" :key="device.id">
+            <v-list-item-icon>
+              <v-icon v-if="device.device_info[0].temperature > 100">mdi-fire</v-icon>
+              <v-icon v-else>{{ device.type === 'GPU' ? 'mdi-expansion-card' : 'mdi-expansion-card-variant' }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ device.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle v-if="$moment(device.device_info[0].time).isBefore($moment().subtract(10, 'minutes'))">
+                Inactive – no current data.
+              </v-list-item-subtitle>
+              <v-list-item-subtitle v-else>
+                Speed: {{ device.device_info[0].speed}} H/s<span v-show="device.device_info[0].temperature >= 0">, Temp: {{ device.device_info[0].temperature}} °C, Util: {{ device.device_info[0].utilization}} %</span>
+              </v-list-item-subtitle>
+              <div class="device-charts">
+                <res-mon
+                  class="chart"
+                  :usage-data="device.device_info.slice(0,30)"
+                  :metrics="['speed']"
+                  :unit-callback="x => `${x} H/s`"
+                />
+                <res-mon
+                  v-show="device.device_info[0].temperature >= 0"
+                  class="chart"
+                  :usage-data="device.device_info.slice(0,30)"
+                  :metrics="['utilization', 'temperature']"
+                  :max="100"
+                  :right-scale="true"
+                  :max-right="100"
+                />
+              </div>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -171,7 +215,7 @@
 
       <fc-tile
         title="Workunits"
-        class="max800 mb-5"
+        class="mb-5"
         :loading="data === null"
       >
         <v-data-table
@@ -214,10 +258,12 @@
   import fmt from '@/assets/scripts/numberFormat'
   import iconv from 'iconv-lite'
   import tile from '@/components/tile/fc_tile.vue'
+  import ResMon from '@/components/chart/resourceMonitor.vue'
   export default {
     name: "HostDetailView",
     components: {
-      'fc-tile': tile
+      'fc-tile': tile,
+      'res-mon': ResMon
     },
     data: function () {
       return {
@@ -335,5 +381,14 @@
     max-width: 800px;
   }
 
+  .device-charts {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .device-charts .chart {
+    flex: 1 0 50%;
+    min-width: 400px;
+  }
 
 </style>
