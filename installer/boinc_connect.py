9d6d106983c36e5d001534ba13dc73ce8b5a5b59
Dávid Bolvanský <david.bolvansky@gmail.com>
2022-09-04 23:40:12 +0200

    enhance boinc_connect with args support

--- installer/boinc_connect.py
+++ installer/boinc_connect.py
@@ -8,7 +8,10 @@ import time
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--url", type=str, default="http://127.0.0.1/fitcrack")
+    parser.add_argument("--url", type=str, default="http://127.0.0.1/fitcrack")
+    parser.add_argument("--email", type=str, default="fitcrack@test.cz")
+    parser.add_argument("--username", type=str, default="fitcrack")
+    parser.add_argument("--password", type=str, default="mypassword")
     args = parser.parse_args()
 
     boinc_dir = None
@@ -23,7 +26,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     subprocess.call(['boinccmd', '--project', args.url, 'detach'], cwd=boinc_dir, stderr=subprocess.DEVNULL)
-    output = subprocess.check_output(['boinccmd', '--create_account', args.url, 'fitcrack@test.cz', 'fitcrack@test.cz', 'fitcrack@test.cz'], cwd=boinc_dir)
+    output = subprocess.check_output(['boinccmd', '--create_account', args.url, args.email, args.password, args.username], cwd=boinc_dir)
     key = output.split()[-1]
     ret = subprocess.call(['boinccmd', '--project_attach', args.url, key], cwd=boinc_dir)
     subprocess.call(['boinccmd', '--project', args.url, 'update'], cwd=boinc_dir)
