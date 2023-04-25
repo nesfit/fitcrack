7e5e1d3c7b0825c496bb61401f9b2189e7a1714f
Dávid Bolvanský <david.bolvansky@gmail.com>
2023-04-25 13:11:34 +0200

    HEX dict indicator in dictionary info UI

--- webadmin/fitcrackAPI/src/src/api/fitcrack/endpoints/dictionary/responseModels.py
+++ webadmin/fitcrackAPI/src/src/api/fitcrack/endpoints/dictionary/responseModels.py
@@ -12,6 +12,7 @@ dictionary_model = api.model('Dictionary', {
     'name': fields.String(readOnly=True, required=False),
     'keyspace': fields.Integer(readOnly=True, required=False),
     'time': fields.DateTime(readOnly=True, required=False),
+    'hex_dict': fields.Boolean(readOnly=True, required=False),
 })
 
 dictionaries_model = api.inherit('Dictionaries collection', {
