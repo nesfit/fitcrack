96e71efe1913209d127c850d0cabedfae7b33a52
Dávid Bolvanský <david.bolvansky@gmail.com>
2023-06-21 09:26:00 +0200

    update migration script

--- server/sql/migration/fitcrack_2_4_1.sql
+++ server/sql/migration/fitcrack_2_4_1.sql
@@ -3,3 +3,6 @@ ADD `dict_deployment_mode` tinyint(3) NOT NULL DEFAULT '0';
 
 ALTER TABLE `fc_workunit`
 ADD`remaining_time` bigint(20) unsigned NOT NULL DEFAULT '0';
+
+ALTER TABLE `fc_job`
+ADD `device_types` int(10) unsigned NOT NULL DEFAULT '1';
